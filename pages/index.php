2b1376855005b49803b649949bf82e90cdb015dd
91RhysG <rhys.gillham.101@outlook.com.au>
2022-10-06 12:53:21 +0800

    Navbar updated
    Navbar is ~80% complete, colours need to be cleaned up and waiting for automated integration with database information.

--- pages/index.php
+++ pages/index.php
@@ -9,17 +9,20 @@ include_once(dirname(__DIR__) . '/shared/head.php');
 
 <body>
     <?php include_once(dirname(__DIR__) . '/shared/navbar.php'); ?>
-    <main>
+    <main class="mainContentAlignment outline">
         <h1>Landing Page</h1>
-        <p>
-            Manager: Kejah Pulman (30034444)
-        </p>
-        <p>
-        <ul> Programmers:
-            <li>Rhys Gillham (M133320)</li>
-            <li>Andrew Masar (P271838) </li>
-        </ul>
-        </p>
+        <div class="container text-center">
+            <p>
+                Manager: Kejah Pulman (30034444)
+            </p>
+            <p>
+            <ul> Programmers:
+                <li>Rhys Gillham (M133320)</li>
+                <li>Andrew Masar (P271838) </li>
+            </ul>
+            </p>
+        </div>
+
     </main>
 </body>
 
--- shared/navbar.php
+++ shared/navbar.php
@@ -10,12 +10,37 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
 <header>
-    <nav>
-        <ul class="d-flex justify-content-center list-format">
-            <li class="p-2"><a href="index.php">Home</a></li>
-            <li class="p-2"><a href="template_table.php">Table Template</a></li>
-        </ul>
+    <nav class="navStyle row ">
+        <div class="col"></div>
+        <div class="col p-2">
+            <ul class="d-flex list-format align-items-center justify-content-center">
+                <li>
+                    <a href=" index.php">
+                        Home
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">Sort by</button>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item">
+                            <a href="template_table.php">Table Template</a>
+                        </li>
+                        <li class="dropdown-item">
+                            <a href="template_table.php">Table Template</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col p-2">
+            <form class="d-flex searchSizing">
+                <input type="search" class="form-control" id="searchValue" placeholder="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+
+
     </nav>
 </header>
