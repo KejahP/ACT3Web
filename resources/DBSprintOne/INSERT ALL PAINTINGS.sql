9b9039b25a1e81ba67c438c2ce72fc5d53bd2113
91RhysG <rhys.gillham.101@outlook.com.au>
2022-10-22 13:44:37 +0800

    Sprint 2 Database Update
    Moved some of the DB files to a new folder called 'DBSprintOne',
    Created files for the changes to use the Artist table,
    Created a new Database back up to restore changes,
    Created an Unknown Artist entry to allow for basic filling of artists.

---
+++ resources/DBSprintOne/INSERT ALL PAINTINGS.sql
@@ -0,0 +1,101 @@
+INSERT INTO paintings 
+(imageFile, name, year, artist, medium, style)
+VALUES 
+(LOAD_FILE('D:/atthelapinagile.png'), 'At the Lapin Agile', 1905, 'Pablo Picasso', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Bal du moulin de la Galette', LOAD_FILE('D:/baldumoulindelagalette.png'), 1876, 'August Renoir', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Cafe Terrace at Night', LOAD_FILE('D:/cafeterraceatnight.png'), 1888, 'Vincent Van Gogh', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Doni Tondo (Doni Madonna)', LOAD_FILE('D:/donitondo.png'), 1507, 'Michelangelo', 'Oil', 'Mannerism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Houses of Parliament', LOAD_FILE('D:/housesofparliament.png'), 1899, 'Claude Monet', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Jaz de Bouffan', LOAD_FILE('D:/jazdebouffan.png'), 1877, 'Paul Cezanne', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Mona Lisa', LOAD_FILE('D:/monalisa.png'), 1503, 'Leonardo da Vinci', 'Oil', 'Portrait')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Nature morte au compotier', LOAD_FILE('D:/naturemorteaucompotier.png'), 1914, 'Pablo Picasso', 'Oil', 'Cubism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Sunrise', LOAD_FILE('D:/sunrise.png'), 1972, 'Claude Monet', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('The Hallucinogenic Toreador', LOAD_FILE('D:/thehallucinogenictoreador.png'), 1970, 'Salvador Dali', 'Oil', 'Surrealism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('The Kingfisher', LOAD_FILE('D:/thekingfisher.png'), 1495, 'Vincent Van Gogh', 'Pen-Ink', 'Realism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('The Persistence of Memory', LOAD_FILE('D:/thepersistenceofmemory.png'), 1931, 'Salvador Dali', 'Oil', 'Surrealism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('The Potato Eaters', LOAD_FILE('D:/thepotatoeaters.png'), 1885, 'Vincent Van Gogh', 'Oil', 'Realism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Vase with Twelve Sunflowers', LOAD_FILE('D:/vasewithtwelvesunflowers.png'), 1888, 'Vincent Van Gogh', 'Oil', 'Still-life')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Vitruvian Man', LOAD_FILE('D:/vitruvianman.png'), 1490, 'Leonardo da Vinci', 'Pen-Ink', 'Realism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Waterlilies and Japanese Bridge', LOAD_FILE('D:/waterliliesandjapanesebridge.png'), 1899, 'Claude Monet', 'Oil', 'Impressionism')
+;
+
+INSERT INTO paintings 
+(name, imageFile, year, artist, medium, style)
+VALUES 
+('Weaver', LOAD_FILE('D:/weaver.png'), 1884, 'Vincent Van Gogh', 'Oil', 'Realism')
+;
