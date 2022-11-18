1d927f86fbea3e114ff53b22985297dd3aaaf467
91RhysG <rhys.gillham.101@outlook.com.au>
2022-11-18 13:45:45 +0800

    Update EmailTable.php
    Table is now capable of searching via email, can be filtered based on newsletter, deleted, and overall traversed.
    
    Cleaned up some of the page, and also have it ordering in the correct way.
    
    It only requires the 1/0 elements to be converted back into checkboxes or similar to display it in a more user friendly way.

--- pages/EmailTable.php
+++ pages/EmailTable.php
@@ -4,33 +4,80 @@
         Kejah Pulman
         30034444
 
+        Rhys Gillham
+        M133320
+
         This is the template for the display of all emails and can only be acessed via an admin login
 -->
+
 <?php
 
 include_once(dirname(__DIR__) . '/script/connection.php');
 include_once(dirname(__DIR__) . '/shared/head.php');
 include_once(dirname(__DIR__) . '/shared/navbar.php');
 
-$sqlEmail = "SELECT * FROM member ORDER BY member.name";
+$sqlEmail = "SELECT * FROM member ORDER BY member.deleteRequest DESC, member.email";
+$breaking = 0;
+$monthly = 0;
+$delete = "None";
+
+if (isset($_POST['search']))
+{
+    $sqlEmail = "SELECT * FROM member WHERE member.email LIKE '%" . $_POST['search'] . "%' ORDER BY member.deleteRequest DESC, member.email";
+}
+elseif (isset($_POST['MonthlyNews']))
+{
+    $sqlEmail = "SELECT * FROM member WHERE member.monthlyNews = 1 ORDER BY member.email";
+}
+elseif (isset($_POST['BreakingNews']))
+{
+    $sqlEmail = "SELECT * FROM member WHERE member.breakingNews = 1 ORDER BY member.email";
+}
+elseif(isset($_POST['Delete'])){
+    $sql = "DELETE FROM member WHERE id =" . $_POST['id'].";";
+    $sql = $conn->prepare($sql);
+    $sql->execute();
+}
 ?>
 
 <body>
     <main class="mainContentAlignment">
 
-        <!--
-        <form class="d-flex searchSizing" action="../script/EmailFiltered.php">
-            <input class="form-control" placeholder="Search" type="search" name="search">
+        <h1>Member Emails</h1>
+
+        <form class="d-flex searchSizing" action="EmailTable.php" method="post">
+            <input class="form-control" placeholder="Email" type="search" name="search">
             <input class="btn" type="submit">
         </form>
--->
-        <h1>Client Emails</h1>
+
         <table class="table">
             <thead>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
-                <th scope="col">Monthly News</th>
-                <th scope="col">Breaking News</th>
+                <th scope="col">
+                    <form class="d-flex searchSizing" action="EmailTable.php" method="post">
+                        <div class="row justify-content-start">
+                            <div class="col-7">
+                                <label for="fMonthly">Monthly News</label>
+                            </div>
+                            <div class="col-3">
+                                <input type="submit" id="fMonthly" name="MonthlyNews" value="V">
+                            </div>
+                        </div>
+                    </form>
+                </th>
+                <th scope="col">
+                    <form class="d-flex searchSizing" action="EmailTable.php" method="post">
+                        <div class="row justify-content-start">
+                            <div class="col-7">
+                                <label for="fBreaking">Breaking News</label>
+                            </div>
+                            <div class="col-3">
+                                <input type="submit" id="fBreaking" name="BreakingNews" value="V">
+                            </div>
+                        </div>
+                    </form>
+                </th>
                 <th scope="col">Delete Requested</th>
             </thead>
 
@@ -47,11 +94,15 @@ $sqlEmail = "SELECT * FROM member ORDER BY member.name";
                 echo "<td scope = \"row\">" . $row['monthlyNews'] . "</td>";
                 echo "<td scope = \"row\">" . $row['breakingNews'] . "</td>";
                 echo "<td scope = \"row\">" . $row['deleteRequest'] . "</td>";
+                echo "<td scope='row'> 
+                        <form action='EmailTable.php' method='post'>
+                        <input hidden type='text' name='Delete' value='delete'>
+                        <input hidden type='text' name='id' value='".$row['id']. "'>
+                            <input class='btn btn-primary' type='submit' value='Delete'>
+                        </form>              
+                    </td>";
                 echo "</tr>";
             }
-
-            //testing of the email page
-            //include_once(dirname(__DIR__) . '/shared/email.php');
 
             ?>
         </table>
